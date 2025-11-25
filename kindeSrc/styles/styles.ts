74c987c2f69b018e5b2a55e49ca0dce3920dfae3
William Hayes <william.hayes-2@student.manchester.ac.uk>
2025-11-25 14:38:59 +0000

    attempt 456

--- kindeSrc/styles/styles.ts
+++ kindeSrc/styles/styles.ts
@@ -13,7 +13,7 @@ const kindeVariables = {
 } as const;
 
 export const getStyles = (): string => `
-  @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&display=swap');
+  @import url(https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&display=swap);
 
   :root {
     --kinde-base-font-family: ${kindeVariables.baseFontFamily};
