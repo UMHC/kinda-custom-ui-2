320d2f60eb253c7202b590b956421668cbe124c6
William Hayes <william.hayes-2@student.manchester.ac.uk>
2025-11-25 15:13:00 +0000

    styles: update Open Sans font family syntax and add unicode-range for better compatibility

--- kindeSrc/styles/styles.ts
+++ kindeSrc/styles/styles.ts
@@ -1,6 +1,6 @@
 // CSS Variables configuration
 const kindeVariables = {
-  baseFontFamily: "'Open Sans', -apple-system, system-ui, BlinkMacSystemFont, Helvetica, Arial, sans-serif",
+  baseFontFamily: "Open Sans, -apple-system, system-ui, BlinkMacSystemFont, Helvetica, Arial, sans-serif",
   controlSelectTextBorderRadius: "99px",
   buttonPrimaryBackgroundColor: "dodgerblue",
   buttonPrimaryColor: "#fff",
@@ -14,27 +14,30 @@ const kindeVariables = {
 
 export const getStyles = (): string => `
   @font-face {
-    font-family: 'Open Sans';
+    font-family: Open Sans;
     font-style: normal;
     font-weight: 400;
     font-display: swap;
     src: url(https://umhc.org.uk/fonts/OpenSans-Regular.woff2) format('woff2');
+    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
   }
 
   @font-face {
-    font-family: 'Open Sans';
+    font-family: Open Sans;
     font-style: normal;
     font-weight: 600;
     font-display: swap;
     src: url(https://umhc.org.uk/fonts/OpenSans-SemiBold.woff2) format('woff2');
+    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
   }
 
   @font-face {
-    font-family: 'Open Sans';
+    font-family: Open Sans;
     font-style: normal;
     font-weight: 700;
     font-display: swap;
     src: url(https://umhc.org.uk/fonts/OpenSans-Bold.woff2) format('woff2');
+    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
   }
 
   :root {
