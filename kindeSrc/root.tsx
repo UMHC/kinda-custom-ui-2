b4a96097961c24b0003c498f8231ca13e981f484
William Hayes <william.hayes-2@student.manchester.ac.uk>
2025-11-25 15:11:23 +0000

    styles: remove unused Open Sans stylesheet and preconnect links

--- kindeSrc/root.tsx
+++ kindeSrc/root.tsx
@@ -51,9 +51,6 @@ export const Root = ({
           type="font/woff2" 
           crossOrigin="anonymous" 
         />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet" />
         {getKindeRequiredCSS()}
         <style>{getStyles()}</style>
         {getKindeRequiredJS()}
